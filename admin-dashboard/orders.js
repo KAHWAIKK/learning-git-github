003e2801938b70621ed659106a1cd4dddb2e4906
Kevin K <kahwaikagwima@gmail.com>
2023-04-24 23:48:12 +0300

    Having Fun with Git Terminal

---
+++ admin-dashboard/orders.js
@@ -0,0 +1,49 @@
+const Orders = [
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber:'85631',
+        paymentStatus: 'Due',
+        shipping:'Pending'
+    },
+    {
+        productName: 'LARVENDER KF102 Drone',
+        productNumber:'36378',
+        paymentStatus: 'Refunded',
+        shipping:'Declined'
+    },
+    {
+        productName: 'Ruko F11 Pro Drone',
+        productNumber:'49347',
+        paymentStatus: 'Due',
+        shipping:'Pending'
+    },
+    {
+        productName: 'Drone with Camera Drone',
+        productNumber:'96996',
+        paymentStatus: 'Due',
+        shipping:'Pending'
+    },
+    {
+        productName: 'GPS with  Drone',
+        productNumber:'22821',
+        paymentStatus: 'Paid',
+        shipping:'Deliverd'
+    },
+    {
+        productName: 'DJI Air 25',
+        productNumber:'81475',
+        paymentStatus: 'Due',
+        shipping:'Pending'
+    },
+    {
+        productName: 'Lozenge Drone',
+        productNumber:'81475',
+        paymentStatus: 'Paid',
+        shipping:'Delivered'
+    }
+]
+    
+
+    
+
+    
